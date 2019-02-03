d2b4430b3ee0b36b657dfb38befc2741dfc6d875
Lutando Ngqakaza <lutando@ngqakaza.com>
2019-02-03 14:59:02 +0100

    final touches

--- src/components/Resume/ResumeBodyBottom.js
+++ src/components/Resume/ResumeBodyBottom.js
@@ -2,15 +2,15 @@ import React from 'react'
 import styled from 'react-emotion'
 
 const Wrapper = styled.section`
-  grid-area: ContactBodyBottom;
+  grid-area: ResumeBodyBottom;
   margin: 0;
   padding: 0 0 2rem;
   height: 100%;
   grid-template-rows: repeat(auto);
 `
 
-const ContactBodyBottom = props => {
+const ResumeBodyBottom = props => {
   return <Wrapper>{props.children}</Wrapper>
 }
 
-export default ContactBodyBottom
+export default ResumeBodyBottom
